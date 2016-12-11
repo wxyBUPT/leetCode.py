d7b8c9dfa73403beb5a2b85e5bbcd9ee8679e164
wxyBUPT <746451950@qq.com>
2016-12-11 12:41:55 +0800

    字符串反转

---
+++ array/Solution_344.py
@@ -0,0 +1,25 @@
+#coding=utf-8
+__author__ = 'xiyuanbupt'
+# e-mail : xywbupt@gmail.com
+
+
+'''
+344. Reverse String   Add to List QuestionEditorial Solution  My Submissions
+Total Accepted: 113564
+Total Submissions: 197829
+Difficulty: Easy
+Contributors: Admin
+Write a function that takes a string as input and returns the string reversed.
+
+Example:
+Given s = "hello", return "olleh".
+'''
+
+class Solution(object):
+
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return s[::-1]
