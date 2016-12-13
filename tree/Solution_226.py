090222b84721c6c9d82d5602a6d011a03301c049
wxyBUPT <746451950@qq.com>
2016-12-13 12:52:15 +0800

    greedy

--- tree/Solution_226.py
+++ tree/Solution_226.py
@@ -46,3 +46,24 @@ class Solution(object):
         root.right = left
         root.left = right
         return root
+
+    '''
+    三行代码解决
+    '''
+    def invertTree(self, root):
+        if root:
+            root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+            return root
+
+    '''
+    使用stack解决
+    '''
+    def invertTree(self, root):
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            if node:
+                node.left, node.right = node.right, node.left
+                stack += node.left, node.right
+        return root
+
