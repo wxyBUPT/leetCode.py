6077714897aff3aaf96ac9f2be9880c19a6c3001
wxyBUPT <746451950@qq.com>
2016-12-21 17:30:00 +0800

    Non-overlapping Intervals

---
+++ greedy/Solution_435.py
@@ -0,0 +1,63 @@
+#coding=utf-8
+__author__ = 'xiyuanbupt'
+# e-mail : xywbupt@gmail.com
+
+'''
+435. Non-overlapping Intervals   Add to List QuestionEditorial Solution  My Submissions
+Total Accepted: 4585
+Total Submissions: 11515
+Difficulty: Medium
+Contributors: love_FDU_llp
+Given a collection of intervals, find the minimum number of intervals you need to remove to make the rest of the intervals non-overlapping.
+
+Note:
+You may assume the interval's end point is always bigger than its start point.
+Intervals like [1,2] and [2,3] have borders "touching" but they don't overlap each other.
+Example 1:
+Input: [ [1,2], [2,3], [3,4], [1,3] ]
+
+Output: 1
+
+Explanation: [1,3] can be removed and the rest of intervals are non-overlapping.
+Example 2:
+Input: [ [1,2], [1,2], [1,2] ]
+
+Output: 2
+
+Explanation: You need to remove two [1,2] to make the rest of intervals non-overlapping.
+Example 3:
+Input: [ [1,2], [2,3] ]
+
+Output: 0
+
+Explanation: You don't need to remove any of the intervals since they're already non-overlapping.
+'''
+class Interval(object):
+    def __init__(self, s=0, e=0):
+        self.start = s
+        self.end = e
+
+class Solution(object):
+
+    # 57.38%
+    def eraseOverlapIntervals(self, intervals):
+        """
+        :type intervals: List[Interval]
+        :rtype: int
+        """
+        def cmp(i_1, i_2):
+            diff = i_1.end - i_2.end
+            if diff:
+                return diff
+            return i_2.start - i_1.start
+        intervals.sort(cmp)
+        end = float('-inf')
+        res = 0
+        for i in intervals:
+            if i.start >= end:
+                end = i.end
+            else:
+                res += 1
+        return res
+
+
