b4decbe1e8ecca8359370562cbede963c10bb839
wxyBUPT <746451950@qq.com>
2017-01-03 10:18:27 +0800

    Palindrome Number

---
+++ leetcodejava/src/main/java/bupt/wxy/math/Solution_9.java
@@ -0,0 +1,24 @@
+package main.java.bupt.wxy.math;
+
+/**
+ * Created by xiyuanbupt on 1/3/17.
+ 9. Palindrome Number   Add to List QuestionEditorial Solution  My Submissions
+ Total Accepted: 173248
+ Total Submissions: 509079
+ Difficulty: Easy
+ Contributors: Admin
+ Determine whether an integer is a palindrome. Do this without extra space.
+ */
+public class Solution_9 {
+
+    public boolean isPalindrome(int x) {
+        if(x<0)return false;
+        int y = 0;
+        int tmp = x;
+        while (x!=0){
+            y=y*10+x%10;
+            x/=10;
+        }
+        return y==tmp;
+    }
+}
