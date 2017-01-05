e385c9702271dfe336e7b31374953942717d8643
wxyBUPT <746451950@qq.com>
2017-01-05 09:47:09 +0800

    二叉树的前序遍历，递归与非递归版本

---
+++ leetcodejava/src/main/java/bupt/wxy/norecursion/PreOrder.java
@@ -0,0 +1,39 @@
+package main.java.bupt.wxy.norecursion;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Created by xiyuanbupt on 1/5/17.
+ */
+public class PreOrder {
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res=new ArrayList<>();
+        preorder(root,res);
+        return res;
+    }
+    void preorder(TreeNode root, List<Integer> res){
+        if(root==null)return;
+        res.add(root.val);
+        preorder(root.left, res);
+        preorder(root.right, res);
+    }
+
+    public List<Integer> preorder(TreeNode root){
+        List<Integer> res=new ArrayList<>();
+        Stack<TreeNode> stack=new Stack<>();
+        TreeNode curr=root;
+        while (!stack.isEmpty()||curr!=null){
+            while (curr!=null){
+                res.add(curr.val);
+                stack.push(curr);
+                curr=curr.left;
+            }
+            curr=stack.pop();
+            curr=curr.right;
+        }
+        return res;
+    }
+}
