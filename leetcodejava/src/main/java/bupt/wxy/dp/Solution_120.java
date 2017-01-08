e58fce0cf318a0df40ce17d9eacd11217f5ec58f
wxyBUPT <746451950@qq.com>
2017-01-08 12:22:04 +0800

    Triangle

---
+++ leetcodejava/src/main/java/bupt/wxy/dp/Solution_120.java
@@ -0,0 +1,52 @@
+package main.java.bupt.wxy.dp;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by xiyuanbupt on 1/8/17.
+ 120. Triangle   Add to List QuestionEditorial Solution  My Submissions
+ Total Accepted: 91376
+ Total Submissions: 281479
+ Difficulty: Medium
+ Contributors: Admin
+ Given a triangle, find the minimum path sum from top to bottom. Each step you may move to adjacent numbers on the row below.
+
+ For example, given the following triangle
+ [
+ [2],
+ [3,4],
+ [6,5,7],
+ [4,1,8,3]
+ ]
+ The minimum path sum from top to bottom is 11 (i.e., 2 + 3 + 5 + 1 = 11).
+ */
+public class Solution_120 {
+
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n;
+        if((n=triangle.size())==0)return 0;
+        int[][] array=new int[n][n];
+        int[][] dp=new int[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i+1;j++){
+                array[i][j]=triangle.get(i).get(j);
+            }
+        }
+
+        dp[0][0]=array[0][0];
+        for(int i=1;i<n;i++){
+            dp[i][0]=dp[i-1][0]+array[i][0];
+            dp[i][i]=dp[i-1][i-1]+array[i][i];
+            for(int j=1;j<i;j++)
+                dp[i][j]=Math.min(dp[i-1][j-1],dp[i-1][j])+array[i][j];
+        }
+        int res=Integer.MAX_VALUE;
+        for(int i=0;i<n;i++){
+            res=Math.min(res,dp[n-1][i]);
+        }
+        return res;
+    }
+}
