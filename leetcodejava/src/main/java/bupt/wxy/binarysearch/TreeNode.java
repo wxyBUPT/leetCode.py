88f86557de110a955b2381cd33486de3a7cbf648
wxyBUPT <746451950@qq.com>
2017-01-12 11:31:38 +0800

    Count Complete Tree Nodes,使用二分法来解决二叉树的问题，二分法不一定要二分的到中间值

---
+++ leetcodejava/src/main/java/bupt/wxy/binarysearch/TreeNode.java
@@ -0,0 +1,14 @@
+package main.java.bupt.wxy.binarysearch;
+
+/**
+ * Created by xiyuanbupt on 1/12/17.
+ */
+public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x){
+        val=x;
+    }
+}
