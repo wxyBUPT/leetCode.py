90982ed1d21651a1f160508d963f162cc055ed95
wxyBUPT <746451950@qq.com>
2017-01-12 22:10:24 +0800

    Population Next Right Pointers in Each Node II

---
+++ leetcodejava/src/main/java/bupt/wxy/dfs/TreeLinkNode.java
@@ -0,0 +1,11 @@
+package main.java.bupt.wxy.dfs;
+
+/**
+ * Created by xiyuanbupt on 1/12/17.
+ */
+public class TreeLinkNode {
+
+    int val;
+    TreeLinkNode left,right,next;
+    TreeLinkNode(int x){val=x;}
+}
