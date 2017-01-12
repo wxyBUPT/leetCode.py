0db3bb6bc076458ce1bc476de54e868e4dc0e483
wxyBUPT <746451950@qq.com>
2017-01-12 11:52:28 +0800

    LRU Cache

---
+++ leetcodejava/src/main/java/bupt/wxy/design/LRUCache.java
@@ -0,0 +1,132 @@
+package main.java.bupt.wxy.design;
+
+/**
+ * Created by xiyuanbupt on 1/12/17.
+ 146. LRU Cache   Add to List QuestionEditorial Solution  My Submissions
+ Total Accepted: 105771
+ Total Submissions: 661846
+ Difficulty: Hard
+ Contributors: Admin
+ Design and implement a data structure for Least Recently Used (LRU) cache. It should support the following operations: get and set.
+
+ get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
+ set(key, value) - Set or insert the value if the key is not already present. When the cache reached its capacity, it should invalidate the least recently used item before inserting a new item.
+
+ Subscribe to see which companies asked this question
+ */
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 实现最近最少使用算, 为了加快页访问速度, map保存key值, 但是为了加快增减删除速度,不应该使用ArrayList ,
+ * 同时为了加快找到前向节点,不应该使用LinkedList,只能使用DoubleList
+ * 这样保证了所有的操作都是O1
+ */
+public class LRUCache {
+    class DLinkedNode {
+        int key;
+        int value;
+        DLinkedNode pre;
+        DLinkedNode post;
+    }
+
+    /**
+     * Always add the new node right after head;
+     */
+    private void addNode(DLinkedNode node){
+        node.pre = head;
+        node.post = head.post;
+
+        head.post.pre = node;
+        head.post = node;
+    }
+
+    /**
+     * Remove an existing node from the linked list.
+     */
+    private void removeNode(DLinkedNode node){
+        DLinkedNode pre = node.pre;
+        DLinkedNode post = node.post;
+
+        pre.post = post;
+        post.pre = pre;
+    }
+
+    /**
+     * Move certain node in between to the head.
+     */
+    private void moveToHead(DLinkedNode node){
+        this.removeNode(node);
+        this.addNode(node);
+    }
+
+    // pop the current tail.
+    private DLinkedNode popTail(){
+        DLinkedNode res = tail.pre;
+        this.removeNode(res);
+        return res;
+    }
+
+    private Map<Integer, DLinkedNode>
+            cache = new HashMap<>();
+    private int count;
+    private int capacity;
+    private DLinkedNode head, tail;
+
+    public LRUCache(int capacity) {
+        this.count = 0;
+        this.capacity = capacity;
+
+        head = new DLinkedNode();
+        head.pre = null;
+
+        tail = new DLinkedNode();
+        tail.post = null;
+
+        head.post = tail;
+        tail.pre = head;
+    }
+
+    public int get(int key) {
+
+        DLinkedNode node = cache.get(key);
+        if(node == null){
+            return -1; // should raise exception here.
+        }
+
+        // move the accessed node to the head;
+        this.moveToHead(node);
+
+        return node.value;
+    }
+
+
+    public void set(int key, int value) {
+        DLinkedNode node = cache.get(key);
+
+        if(node == null){
+
+            DLinkedNode newNode = new DLinkedNode();
+            newNode.key = key;
+            newNode.value = value;
+
+            this.cache.put(key, newNode);
+            this.addNode(newNode);
+
+            ++count;
+
+            if(count > capacity){
+                // pop the tail
+                DLinkedNode tail = this.popTail();
+                this.cache.remove(tail.key);
+                --count;
+            }
+        }else{
+            // update the value.
+            node.value = value;
+            this.moveToHead(node);
+        }
+
+    }
+}
