b370c0d5d03afe37d47b0c537dfa5b9cc172860c
wxyBUPT <746451950@qq.com>
2017-01-12 15:46:51 +0800

    Insert Delete GetRandom O(1)

--- leetcodejava/src/main/java/bupt/wxy/design/RandomizedSet.java
+++ leetcodejava/src/main/java/bupt/wxy/design/RandomizedSet.java
@@ -52,19 +52,19 @@ import java.util.Map;
 public class RandomizedSet {
 
     ArrayList<Integer> nums;
-    Map<Integer, Integer> locs;
+    Map<Integer,Integer> locs;
     java.util.Random rand = new java.util.Random();
     /** Initialize your data structure here. */
     public RandomizedSet() {
-        nums = new ArrayList<Integer>();
+        nums = new ArrayList<>();
         locs = new HashMap<>();
     }
 
     /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
     public boolean insert(int val) {
-        boolean contain = locs.containsKey(val);
-        if ( contain ) return false;
-        locs.put( val, nums.size());
+        boolean contain=locs.containsKey(val);
+        if(contain)return false;
+        locs.put(val,nums.size());
         nums.add(val);
         return true;
     }
@@ -72,20 +72,20 @@ public class RandomizedSet {
     /** Removes a value from the set. Returns true if the set contained the specified element. */
     public boolean remove(int val) {
         boolean contain = locs.containsKey(val);
-        if ( ! contain ) return false;
+        if (!contain) return false;
         int loc = locs.get(val);
-        if (loc < nums.size() - 1 ) { // not the last one than swap the last one with this val
-            int lastone = nums.get(nums.size() - 1 );
-            nums.set( loc , lastone );
-            locs.put(lastone, loc);
+        if (loc<nums.size()- 1) { // not the last one than swap the last one with this val
+            int lastone = nums.get(nums.size()-1);
+            nums.set(loc,lastone);
+            locs.put(lastone,loc);
         }
         locs.remove(val);
-        nums.remove(nums.size() - 1);
+        nums.remove(nums.size()-1);
         return true;
     }
 
     /** Get a random element from the set. */
     public int getRandom() {
-        return nums.get( rand.nextInt(nums.size()) );
+        return nums.get(rand.nextInt(nums.size()));
     }
 }
