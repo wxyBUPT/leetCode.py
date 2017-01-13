0e492192107e981cb9ac454e234897c1e92be575
wxyBUPT <746451950@qq.com>
2017-01-13 16:01:43 +0800

    Implement Queue using Stacks

---
+++ leetcodejava/src/main/java/bupt/wxy/stack/MyQueue.java
@@ -0,0 +1,53 @@
+package main.java.bupt.wxy.stack;
+
+import java.util.Stack;
+
+/**
+ * Created by xiyuanbupt on 1/13/17.
+ 232. Implement Queue using Stacks   Add to List QuestionEditorial Solution  My Submissions
+ Total Accepted: 68410
+ Total Submissions: 195596
+ Difficulty: Easy
+ Contributors: Admin
+ Implement the following operations of a queue using stacks.
+
+ push(x) -- Push element x to the back of queue.
+ pop() -- Removes the element from in front of queue.
+ peek() -- Get the front element.
+ empty() -- Return whether the queue is empty.
+ Notes:
+ You must use only standard operations of a stack -- which means only push to top, peek/pop from top, size, and is empty operations are valid.
+ Depending on your language, stack may not be supported natively. You may simulate a stack by using a list or deque (double-ended queue), as long as you use only standard operations of a stack.
+ You may assume that all operations are valid (for example, no pop or peek operations will be called on an empty queue).
+ */
+public class MyQueue {
+
+    // 61.89%
+    Stack<Integer> left=new Stack<>();
+    Stack<Integer> right=new Stack<>();
+    // Push element x to the back of queue.
+    public void push(int x) {
+        left.push(x);
+    }
+
+    // Removes the element from in front of queue.
+    public void pop() {
+        peek();
+        right.pop();
+    }
+
+    // Get the front element.
+    public int peek() {
+        if(right.isEmpty()){
+            while (!left.isEmpty()){
+                right.push(left.pop());
+            }
+        }
+        return right.peek();
+    }
+
+    // Return whether the queue is empty.
+    public boolean empty() {
+        return left.isEmpty()&&right.isEmpty();
+    }
+}
