bfe4fb52c7abebffd2092a0a0375e09d788c2762
wxyBUPT <746451950@qq.com>
2017-01-13 10:40:46 +0800

    Merge k Sorted Lists

---
+++ leetcodejava/src/main/java/bupt/wxy/heap/ListNode.java
@@ -0,0 +1,13 @@
+package main.java.bupt.wxy.heap;
+
+/**
+ * Created by xiyuanbupt on 1/13/17.
+ */
+public class ListNode {
+
+    int val;
+    ListNode next;
+    ListNode(int x){
+        val=x;
+    }
+}
---
+++ leetcodejava/src/main/java/bupt/wxy/heap/Solution_23.java
@@ -0,0 +1,42 @@
+package main.java.bupt.wxy.heap;
+
+/**
+ * Created by xiyuanbupt on 1/13/17.
+ 23. Merge k Sorted Lists   Add to List QuestionEditorial Solution  My Submissions
+ Total Accepted: 124375
+ Total Submissions: 479365
+ Difficulty: Hard
+ Contributors: Admin
+ Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+ */
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * 有时间看看败者树的实现, 败者树的实现要比mergesort 复杂很多
+ */
+public class Solution_23 {
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists.length==0)return null;
+        PriorityQueue<ListNode> heap=new PriorityQueue<>(lists.length, new Comparator<ListNode>() {
+            @Override
+            public int compare(ListNode o1, ListNode o2) {
+                return o1.val-o2.val;
+            }
+        });
+
+        ListNode head_pre=new ListNode(0);
+        ListNode tail=head_pre;
+        for(ListNode node:lists){
+            if(node!=null)heap.add(node);
+        }
+        while (!heap.isEmpty()){
+            tail.next=heap.poll();
+            tail=tail.next;
+            if(tail.next!=null)heap.add(tail.next);
+        }
+        return head_pre.next;
+    }
+}
