72893a27ef5596e23d1c8b6a9383d19ef2ccab64
wxyBUPT <746451950@qq.com>
2017-02-02 14:10:17 +0800

    Implement_strStr

---
+++ leetcodejava/src/main/java/bupt/wxy/twopoint/Implement_strStr.java
@@ -0,0 +1,36 @@
+package main.java.bupt.wxy.twopoint;
+
+/**
+ * Created by xiyuanbupt on 2/2/17.
+ 28. Implement strStr()   Add to List QuestionEditorial Solution  My Submissions
+ Total Accepted: 155718
+ Total Submissions: 574958
+ Difficulty: Easy
+ Contributors: Admin
+ Implement strStr().
+
+ Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+ */
+public class Implement_strStr {
+
+    /**
+     * find subString
+     * 51.91%
+     * @param haystack
+     * @param needle
+     * @return
+     */
+    public int strStr(String haystack, String needle) {
+        int i=0,j;
+        while (i<=haystack.length()-needle.length()){
+            int k=i;
+            j=0;
+            while (j<needle.length()&&haystack.charAt(k++)==needle.charAt(j)){
+                j++;
+            }
+            if(j==needle.length())return i;
+            i++;
+        }
+        return -1;
+    }
+}
