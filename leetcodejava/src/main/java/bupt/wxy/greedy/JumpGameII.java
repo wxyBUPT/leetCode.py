cc3aa80802abaf42d590d68c82f5a06b61ca947b
wxyBUPT <746451950@qq.com>
2017-02-02 14:09:19 +0800

    JumpGameII

---
+++ leetcodejava/src/main/java/bupt/wxy/greedy/JumpGameII.java
@@ -0,0 +1,46 @@
+package main.java.bupt.wxy.greedy;
+
+/**
+ * Created by xiyuanbupt on 2/2/17.
+ 45. Jump Game II   Add to List QuestionEditorial Solution  My Submissions
+ Total Accepted: 83383
+ Total Submissions: 320461
+ Difficulty: Hard
+ Contributors: Admin
+ Given an array of non-negative integers, you are initially positioned at the first index of the array.
+
+ Each element in the array represents your maximum jump length at that position.
+
+ Your goal is to reach the last index in the minimum number of jumps.
+
+ For example:
+ Given array A = [2,3,1,1,4]
+
+ The minimum number of jumps to reach the last index is 2. (Jump 1 step from index 0 to 1, then 3 steps to the last index.)
+ */
+public class JumpGameII {
+
+    /**
+     * 21.05%
+     * @param nums
+     * @return
+     */
+    public int jump(int[] nums) {
+        if(nums.length<2)return 0;
+        int res=0;
+        int curr=0;
+
+        int max_reach=nums[0];
+        while (max_reach+1<nums.length){
+            res++;
+            int tmp=curr;
+            for(int i=1;i<=nums[tmp];i++){
+                if(tmp+i+nums[tmp+i]>max_reach){
+                    curr=tmp+i;
+                    max_reach=tmp+i+nums[tmp+i];
+                }
+            }
+        }
+        return res+1;
+    }
+}
