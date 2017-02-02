83a9fd08a508fedbc7fd7c997d108912b89388c9
wxyBUPT <746451950@qq.com>
2017-02-02 14:09:55 +0800

    ValidParentheses

---
+++ leetcodejava/src/main/java/bupt/wxy/stack/ValidParentheses.java
@@ -0,0 +1,49 @@
+package main.java.bupt.wxy.stack;
+
+import java.util.Stack;
+
+/**
+ * Created by xiyuanbupt on 2/2/17.
+ 20. Valid Parentheses   Add to List QuestionEditorial Solution  My Submissions
+ Total Accepted: 167275
+ Total Submissions: 517399
+ Difficulty: Easy
+ Contributors: Admin
+ Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+ The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+ */
+public class ValidParentheses {
+
+    /**
+     * Judge string is valid Parentheses
+     * 75.69%
+     * @param s
+     * @return
+     */
+    public boolean isValid(String s) {
+        char[] chars=s.toCharArray();
+        Stack<Character> stack=new Stack<>();
+        for(char c:chars){
+            switch (c){
+                case '[':
+                case '{':
+                case '(':
+                    stack.push(c);
+                    break;
+                case '}':
+                    if(stack.isEmpty()||stack.pop()!='{')return false;
+                    break;
+                case ']':
+                    if(stack.isEmpty()||stack.pop()!='[')return false;
+                    break;
+                case ')':
+                    if(stack.isEmpty()||stack.pop()!='(')return false;
+                    break;
+                default:
+                    break;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
